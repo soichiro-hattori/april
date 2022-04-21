a66df39689433434fe93c9591356b701741c4715
Soichiro Hattori <soichiro.hattori@gmail.com>
2022-04-21 16:34:56 -0400

    test pre-commit

--- setup.py
+++ setup.py
@@ -14,9 +14,5 @@ setup(
     license="MIT",
     description="A lightweight Python package to download and interact with ZTF light curve data",
     long_description=open("README.rst").read(),
-    install_requires=[
-        "numpy>=1.20.2",
-        "astropy>=4.2.1",
-        "requests>=2.25.1"
-    ],
+    install_requires=["numpy>=1.20.2", "astropy>=4.2.1", "requests>=2.25.1"],
 )
